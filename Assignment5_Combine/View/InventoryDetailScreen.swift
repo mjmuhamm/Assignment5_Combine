71bdc2fad39857b4ecf4e028d4768697361ef6f2
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-03-24 08:24:32 -0400

    additions

--- Assignment5_Combine/View/InventoryDetailScreen.swift
+++ Assignment5_Combine/View/InventoryDetailScreen.swift
@@ -10,42 +10,49 @@ import SwiftUI
 struct InventoryDetailScreen: View {
     let product : Inventory
     var body: some View {
-        VStack(alignment: .center) {
-            
-            AsyncImage(url: URL(string: product.image)) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(8)
-                    .frame(width: 250, height: 250)
-            } placeholder: {
-                Image(systemName: "person.crop.circle.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
-                    .foregroundColor(.gray)
-            }.padding(.bottom, 9)
-            
-            Text(product.title)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .fixedSize(horizontal: false, vertical: true)
-                .frame(maxWidth: .infinity, alignment: .center)
-                .padding(.bottom, 1.5)
-            
-            Text(String(format: "$%.2f", product.price))
-            
-        }.padding(10)
-        
         VStack(alignment: .leading) {
-            Text("Id: \(product.id)").padding(.top, 0.2)
-            Text("Description: \(product.description)").padding(.top, 0.2)
-            Text("Category: \(product.category)").padding(.top, 0.2)
-            Text("Rating: \(product.rating.rate, specifier: "%.2f")").padding(.top, 0.2)
             
-        }.padding(10).frame(alignment: .topLeading)
+            VStack(alignment: .center) {
+                
+                AsyncImage(url: URL(string: product.image)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(8)
+                        .frame(width: 250, height: 250)
+                } placeholder: {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                        .foregroundColor(.gray)
+                }.padding(.bottom, 9)
+                
+                Text(product.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(.bottom, 1.5)
+                
+                Text(String(format: "$%.2f", product.price))
+                
+            }.padding(10)
+            
+            VStack(alignment: .leading) {
+                Text("Id: \(product.id)").padding(.top, 0.2)
+                Text("Description: \(product.description)").padding(.top, 0.2)
+                Text("Category: \(product.category)").padding(.top, 0.2)
+                Text("Rating: \(product.rating.rate, specifier: "%.2f")").padding(.top, 0.2)
+                
+            }.padding(.horizontal, 35).padding(.top).frame(alignment: .topLeading)
+        }
+            
+            VStack(alignment: .center) {
+                
+                Text("* \(product.rating.count) ratings of this product *").padding(.top).foregroundStyle(.gray).font(.system(size: 15))
+            }
         
-        Text("* \(product.rating.count) ratings of this product *").padding(.top, 0.5).foregroundStyle(.gray).font(.system(size: 15))
     }
 }
 
